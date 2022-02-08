6ab045fcf020eec167f54470974d5767646f9b8d
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2022-02-09 04:39:32 +0530

    adding more func to checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,29 +1,39 @@
- import React from 'react';
- import "./Checkout.css"
-import Subtotal from './Subtotal';
+import React from "react";
+import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { getBasketTotal } from "./reducer";
+import { useStateValue } from "./StateProvider";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
-  return( 
-    <div className='checkout'>
-        <div className='checkout__left'>
-            <img className='checkout__ad' 
-            src="https://images-na.ssl-images-amazon.com/images/G/01/prime/PrimeDeals/Test/vxd-737_Prime_Deals_Test_OS_SP_Hero_v1_3000x600_2x.jpg">
-            </img>
+  const [{ basket }, dispatch] = useStateValue();
 
-            <div>
-                <h2 className='checkout__title'>
-                    Your shopping Basket</h2>
-            </div>
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/prime/PrimeDeals/Test/vxd-737_Prime_Deals_Test_OS_SP_Hero_v1_3000x600_2x.jpg"
+        ></img>
+
+        <div>
+          <h2 className="checkout__title"> Your shopping Basket</h2>
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
+      </div>
 
-        <div className='checkout__right'>
-            <Subtotal />
-        </div>
-
-
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
     </div>
-
-
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,7 @@ function Home() {
                 <Product
                     id = "12345387" 
                     title = "Atomic Habits: The life-changing million copy bestseller Paperback"
-                    price = {"541"}
+                    price = {541}
                     image = "https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg"
                     rating = {3}
                 />
@@ -21,7 +21,7 @@ function Home() {
                 <Product 
                     id = "1732642"
                     title = "boAt Xtend Smartwatch with Alexa Built-in, 1.69” HD Display & 5 ATM Water Resistance(Pitch Black)"
-                    price = {"2,999"}
+                    price = {2999}
                     image = "https://m.media-amazon.com/images/I/61IMRs+o0iL._SL1500_.jpg"
                     rating = {4}
                 />  
@@ -32,7 +32,7 @@ function Home() {
                 <Product 
                     id = "12345389"
                     title = "Apple iPhone 13 Pro (256GB) - Sierra Blue"
-                    price = {"1,29,900"}
+                    price = {129900}
                     image = "https://m.media-amazon.com/images/I/61jLiCovxVL._SL1500_.jpg"
                     rating = {4}
                 />
@@ -40,14 +40,14 @@ function Home() {
                 <Product 
                     id = "126742"
                     title = "Echo (4th Gen, 2020 release) | Powered by Dolby and Alexa (Black)"
-                    price = {"6,999.0"}
+                    price = {6999}
                     image = "https://m.media-amazon.com/images/I/61-DjUz7JxL.jpg"
                     rating = {5}
                 />
                 <Product 
                     id = "1545642"
                     title = "Cadbury Dairy Milk Silk Fruit and Nut Chocolate Bar, 137g (Pack of 3)"
-                    price = {"483"}
+                    price = {483}
                     image = "https://m.media-amazon.com/images/I/61QNTafi2iL._SL1500_.jpg"
                     rating = {4}
                 />
@@ -58,7 +58,7 @@ function Home() {
                 <Product 
                     id = "1234522"
                     title = "Samsung 34-inch (86.40cm) Curved Monitor- 21:9 Ultrawide QLED, Thunderbolt 3 Port- LC34J791WTWXXL, Gray"
-                    price = {"85,600"}
+                    price = {85600}
                     image = "https://m.media-amazon.com/images/I/91pi34PiUPL._SL1500_.jpg"
                     rating = {4}
                 />
