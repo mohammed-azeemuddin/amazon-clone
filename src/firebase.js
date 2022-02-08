fa5f7b8d0e7615fe9434f35bb31ef2be156ddd1e
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2022-02-08 21:39:22 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAzRyLyTez77tUeHRy97CKAy-GVG90P6Xg",
+    authDomain: "challenge-d6288.firebaseapp.com",
+    projectId: "challenge-d6288",
+    storageBucket: "challenge-d6288.appspot.com",
+    messagingSenderId: "1024865075975",
+    appId: "1:1024865075975:web:2352f9e275eccbf2093ba2"
+  };
